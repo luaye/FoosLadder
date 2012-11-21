d78b85643006a482e3e0d62b67b9242b0d8222d2
LuAye <luaye85@gmail.com>
2012-11-21 22:26:47 +0000

    sizing.

--- static/js/playersView.js
+++ static/js/playersView.js
@@ -26,7 +26,7 @@ this.show = function()
 		commentsInited = true;
 		var element = $(table.element);
 		var commentArea = element.find(".commentArea");
-		var width = element.innerWidth() - 20;
+		var width = element.innerWidth() - 15;
 		commentArea.replaceWith('<div class="fb-comments" data-href="http://foos.apelabs.net#players" data-num-posts="4" data-width="'+width+'" mobile="false"></div>');
 		FB.XFBML.parse(commentArea[0]);
 	}
