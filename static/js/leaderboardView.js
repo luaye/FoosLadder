db06f5bc5cf4afab1d6010ad3c14e76779a0ef19
sorran <andrew@spaceapegames.com>
2016-03-03 15:20:17 +0000

    leaderboard views

--- static/js/leaderboardView.js
+++ static/js/leaderboardView.js
@@ -1,13 +1,14 @@
 var tableData = [];
 var tableName;
 
-var tables = ["orca1_race_data_20160302"];
+var tables = ["orca1_player_data_20160302"];
 var raceTableView;
 var playersTableView;
 
 function init()
 {
 	raceTableView = $('#race-table');
+	$('#more').hide();
 	playersTableView = $('#players-table');
 	hideTables();
 	$('#leaderboardName').val(tables[0]);
@@ -32,21 +33,28 @@ function init()
 			  search: false
 			}
 	});
+
 }
 
+function showMore(){
+	$('#more').show();
+}
 function hideTables(){
 		raceTableView.hide();
 		playersTableView.hide();
+		$('#raceContent').hide();
+		$('#playerContent').hide();
 }
 
 function onViewLeaderboard()
 {
-	hideTables();
-	loadTableData();
+	loadTableData($('#leaderboardName').val());
 }
-function loadTableData()
+
+function loadTableData(leaderboardName)
 {
-	tableName = $('#leaderboardName').val();
+	hideTables();
+	tableName = leaderboardName;
 	tableData = [];
 
 	if (tableName.indexOf("race_data") > 0){
@@ -64,10 +72,41 @@ function loadTableData()
 
 function onTableLoaded(data)
 {
-	tableData = data.concat();
+	if (activeTable == playersTableView){
+		tableData = [];
+		for(var i = 0; i < data.length; i++) {
+			var blob = JSON.parse(data[i].blob);
+			var leaderboardItem = {
+					"Name": blob.Name,
+					"UID": blob.UID,
+					"LastModifiedDate": blob.LastModifiedDate
+			};
+
+			var totalLevel = 0;
+			for(var j = 0; blob.ClassData != null && j < blob.ClassData.length; j++) {
+				var classItem = blob.ClassData[j];
+				totalLevel += classItem.Level;
+				leaderboardItem[classItem.classId + "Level"] = classItem.Level;
+			}
+			leaderboardItem["PlayerLevel"] = totalLevel
+
+			tableData = tableData.concat(leaderboardItem);
+			var sortKey = "PlayerLevel";
+			var sortDir = -1;
+			$('#playerContent').show();
+
+		}
+	}else{
+		tableData = data.concat();
+		var sortKey = "raceDate";
+		var sortDir = -1;
+		$('#raceContent').show();
+	}
 
 	var dynatable = activeTable.data('dynatable')
 	dynatable.settings.dataset.originalRecords = tableData;
+	dynatable.sorts.clear();
+	dynatable.sorts.add(sortKey,sortDir);
   	dynatable.process();
   	activeTable.show();
 }
